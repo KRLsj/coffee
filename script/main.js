e5c782a033349039cdbd5531cc376e54777277b3
KRLsj <104981643+KRLsj@users.noreply.github.com>
2022-10-07 17:40:12 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,59 +1,80 @@
-/* 292.p*/
-const badgeEl= document.querySelector('.badges');
+// 292.p
+const badgeEl = document.querySelector('.badges');
+const topBtnEl = document.querySelector('.top_btn');
+
+topBtnEl.addEventListener('click', function(){
+  //테스트용 -> alert('top버튼 클릭 잘 되요.');
+  gsap.to(window, .6, { //브라우저를 선택하여 0.6초만에
+    scrollTo: 0 //스크롤 꼭때기 0까지 올라간다.
+  });
+});
 
 window.addEventListener('scroll', function(){
-    console.log(window.scrollY); //스크롤값 확인하기
-    if(760<window.scrollY){
-        // badgeEl.style.display='none';
-        gsap.to(badgeEl,0.6,{
-            opacity:0,
-            display:'none'
-        })
-
-    }else{
-        // badgeEl.style.display='block';
-        gsap.to(badgeEl,0.6,{
-            opacity:1,
-            display:'block'
-        })
-    }
+  console.log(window.scrollY); //스크롤값 확인하기
+  if(700<this.window.scrollY){
+    // badgeEl.style.display='none';
+    gsap.to(badgeEl, 0.6,{opacity:0, display:'none'});
+    gsap.to(topBtnEl, 0.6,{opacity:1});
+  }else{
+    // badgeEl.style.display='block';
+    gsap.to(badgeEl, 0.6,{opacity:1, display:'block'});
+    gsap.to(topBtnEl, 0.6,{opacity:0});
+  }
 });
 
-/*gsap.to(요소,시간,속성)*/ 
+// gsap.to(요소, 시간, 속성)
 
-const fadeEl=document.querySelectorAll('.fadein');
+const fadeEl = document.querySelectorAll('.fadeIn');
 
-fadeEl.forEach(function(fadeEl,index){
- gsap.to(fadeEl,1,{
-    delay:(index+1)*0.7,//0.7초씩 느리게 시간을 추가
+fadeEl.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay:(index+1)*0.7, //0.7초씩 느리게 시간을 추가
     opacity:1
+  });
 });
+
+// 커피 프로모션 토글아이콘 클릭시 박스 숨기고/나타내기
+const promotionEl = document.querySelector ('section.promotion');
+const toggleBtnEl = document.querySelector ('.toggle_btn');
+
+// 토글버튼을 클릭하면 프로모션의 크기가 변한다.
+toggleBtnEl.addEventListener('click', function(){
+  // alert('sgbdsb');
+
+  if(promotionEl.classList.contains('hide')){ //만약 promotion클래스에 hide서식이 있다면
+    // 높이 663이되어야
+    promotionEl.classList.remove('hide');
+  }else{  //그렇지 않으면
+    // 높이가 0
+    promotionEl.classList.add('hide');
+  }
 });
 
-// const promotionEl= document.querySelector("section.promotion");
-// const promotionToggleBtn= document.querySelector(".n_inner__right>.material-icons");
-// //토글 클릭시
-// promotionToggleBtn.addEventListener('click',function(){
-//     if(promotionEl.classList.contains('hide')) {
-//         promotionEl.classList.remove('hide');
-//     }else{
-//         promotionEl.classList.add('hide');
-//     }
-// });
 
-//커피프로모션 토글아이콘 클릭시 박스 숨기고/ 나타내기
+// 6, 7, 8, 10 콘텐츠가 화면의 80% 지점에 보일 때 동작하는 스크립트
+const spyEl = document.querySelectorAll('section.scroll-spy');
 
-const promotionEl = document.querySelector('section.promotion');
-const toggleBtnEl = document.querySelector('.toggle_btn');
+spyEl.forEach(function(spyEl){
+  new ScrollMagic
+  .Scene({ //감시할 장면을 추가
+    triggerElement:spyEl,
+    triggerHook:0.8 //화면의 80% 지점에서 동작
+  })
+  .setClassToggle(spyEl, 'show') //show클래스를 적용하거나 해제하기
+  .addTo(new ScrollMagic.Controller()); //컨트롤러에 장면을 할당한다.
+});
 
-//토글버튼을 클릭하면 프로모션의 크기가 변한다.
+// 푸터 카피라이트
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
 
-toggleBtnEl.addEventListener('click',function(){
-    if(promotionEl.classList.contains('hide')){//만약에 promotion클래스에 hide서식이 있다면 조건에 맞으면
-        promotionEl.classList.remove('hide')//높이 663이되어야
-    }else{//그렇지 않으면
-        promotionEl.classList.add('hide')//높이가 0원래대로
+new Swiper('.awards .swiper', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-button-prev',
+        nextEl: '.awards .swiper-button-next'
     }
 });
-
-
